16725865f6706986ef6febcd00d979cdcc101c41
MANISHA <manishapanchal169@gmail.com>
2022-08-05 10:25:00 +0530

    feat: :sparkles: implement searchbar in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import React from "react";
-
+import SearchBar from "./components/SearchBar/SearchBar";
 const App = () => {
-  return <div>hello world</div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 };
 
 export default App;
