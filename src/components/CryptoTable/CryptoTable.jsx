a9cb63ee4ace31a21f8ce7396c58568581391a3c
MANISHA <manishapanchal169@gmail.com>
2022-08-10 10:15:18 +0530

    feat: :sparkles: add  sorting by price , mkt. capital and % change
    by asc. desc. & default

--- src/components/CryptoTable/CryptoTable.jsx
+++ src/components/CryptoTable/CryptoTable.jsx
@@ -3,10 +3,30 @@ import { Table } from "antd";
 import { Select } from "antd";
 import axios from "axios";
 import "./CryptoTable.css";
-import { Option } from "antd/lib/mentions";
+
+const bubbleSort = (arr, type) => {
+  for (let i = 0; i < arr.length; i++) {
+    // Last i elements are already in place
+    for (let j = 0; j < arr.length - i - 1; j++) {
+      const condition =
+        type === "asc"
+          ? arr[j].currPrice > arr[j + 1].currPrice
+          : arr[j].currPrice < arr[j + 1].currPrice;
+      // Checking if the item at present iteration
+      // is greater than the next iteration
+      if (condition) {
+        // If the condition is true then swap them
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+};
 
 const CryptoTable = () => {
   const [coinData, setCoinData] = useState([]);
+  const [inputVal, setInputVal] = useState("");
   const [filteredCoinData, setFilteredCoinData] = useState([]);
   const { Option } = Select;
   const columns = [
@@ -38,13 +58,29 @@ const CryptoTable = () => {
     setCoinData(coinDataArr);
   };
 
-  const handleChange = (e) => {
+  const filterData = () => {
     const filteredData = coinData.filter((currentCoin) => {
-      return currentCoin.name
-        .toLowerCase()
-        .includes(e.target.value.toLowerCase());
+      return currentCoin.name.toLowerCase().includes(inputVal.toLowerCase());
     });
     setFilteredCoinData(filteredData);
+  };
+
+  const handleChange = (e) => {
+    setInputVal(e.target.value);
+  };
+
+  const handlePriceChange = (value) => {
+    const newFilteredCoinData = [...filteredCoinData];
+
+    if (value === "LowestFirst") {
+      bubbleSort(newFilteredCoinData, "asc");
+      setFilteredCoinData(newFilteredCoinData);
+    } else if (value === "HighestFirst") {
+      bubbleSort(newFilteredCoinData, "des");
+      setFilteredCoinData(newFilteredCoinData);
+    } else {
+      filterData();
+    }
   };
 
   useEffect(() => {
@@ -55,6 +91,10 @@ const CryptoTable = () => {
     setFilteredCoinData(coinData);
   }, [coinData]);
 
+  useEffect(() => {
+    filterData();
+  }, [inputVal]);
+
   return (
     <div>
       <div className="search">
@@ -62,27 +102,29 @@ const CryptoTable = () => {
           type="search"
           placeholder="Enter the coin name"
           onChange={handleChange}
+          value={inputVal}
           className="input"
           autoFocus
         />
+        <div>
+          <Select defaultValue="CurrentPrice" onChange={handlePriceChange}>
+            <Option value="CurrentPrice">Current Price</Option>
+            <Option value="LowestFirst">Lowest First</Option>
+            <Option value="HighestFirst">Highest First</Option>
+          </Select>
+          <Select defaultValue="Market Capital">
+            <Option value="MarketCapital">Market Capital</Option>
+            <Option value="LowestFirst">Lowest First</Option>
+            <Option value="HighestFirst">Highest First</Option>
+          </Select>
+          <Select defaultValue="% Change">
+            <Option value="%Change">% Change</Option>
+            <Option value="LowestFirst">Lowest First</Option>
+            <Option value="HighestFirst">Highest First</Option>
+          </Select>
+        </div>
       </div>
-      <div>
-        <Select defaultValue="Current Price">
-          <Option value="Current Price">Current Price</Option>
-          <Option value="Lowest First">Lowest First</Option>
-          <Option value="Highest First">Highest First</Option>
-        </Select>
-        <Select defaultValue="Market Capital">
-          <Option value="Market Capital">Market Capital</Option>
-          <Option value="Lowest First">Lowest First</Option>
-          <Option value="Highest First">Highest First</Option>
-        </Select>
-        <Select defaultValue="% Change">
-          <Option value="% Change">% Change</Option>
-          <Option value="Lowest First">Lowest First</Option>
-          <Option value="Highest First">Highest First</Option>
-        </Select>
-      </div>
+
       <Table columns={columns} dataSource={filteredCoinData} />
     </div>
   );
