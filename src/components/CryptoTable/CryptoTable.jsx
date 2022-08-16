353351799733c0bf4a59bc696ab7945bf6b14e91
MANISHA <manishapanchal169@gmail.com>
2022-08-16 11:53:11 +0530

    refactor: :recycle:

--- src/components/CryptoTable/CryptoTable.jsx
+++ src/components/CryptoTable/CryptoTable.jsx
@@ -10,67 +10,7 @@ import "./CryptoTable.css";
 
 ReactFusioncharts.fcRoot(FusionCharts, Charts, FusionTheme);
 const ReactFC = React.lazy(() => import("react-fusioncharts"));
-const dataSource = {
-  chart: {
-    caption: "Average Fastball Velocity",
-    yaxisname: "Velocity (in mph)",
-    subcaption: "[2005-2016]",
-    numbersuffix: " mph",
-    rotatelabels: "1",
-    setadaptiveymin: "1",
-    theme: "fusion",
-  },
-  data: [
-    {
-      label: "2005",
-      value: "89.45",
-    },
-    {
-      label: "2006",
-      value: "89.87",
-    },
-    {
-      label: "2007",
-      value: "89.64",
-    },
-    {
-      label: "2008",
-      value: "90.13",
-    },
-    {
-      label: "2009",
-      value: "90.67",
-    },
-    {
-      label: "2010",
-      value: "90.54",
-    },
-    {
-      label: "2011",
-      value: "90.75",
-    },
-    {
-      label: "2012",
-      value: "90.8",
-    },
-    {
-      label: "2013",
-      value: "91.16",
-    },
-    {
-      label: "2014",
-      value: "91.37",
-    },
-    {
-      label: "2015",
-      value: "91.66",
-    },
-    {
-      label: "2016",
-      value: "91.8",
-    },
-  ],
-};
+
 // if value is in Billion format then remove B
 // convert String to number
 // then do comparison
@@ -108,6 +48,76 @@ const CryptoTable = () => {
   const [isVisible, setIsVisible] = useState(false);
   const [selectedCoin, setSelectedCoin] = useState({});
   const { Option } = Select;
+
+  const dataSource = {
+    chart: {
+      caption: "Average Fastball Velocity",
+      yaxisname: "Velocity (in mph)",
+      subcaption: "[2005-2016]",
+      numbersuffix: " mph",
+      rotatelabels: "1",
+      setadaptiveymin: "1",
+      theme: "fusion",
+    },
+    data: [
+      {
+        label: "2005",
+        value: "89.45",
+      },
+      {
+        label: "2006",
+        value: "89.87",
+      },
+      {
+        label: "2007",
+        value: "89.64",
+      },
+      {
+        label: "2008",
+        value: "90.13",
+      },
+      {
+        label: "2009",
+        value: "90.67",
+      },
+      {
+        label: "2010",
+        value: "90.54",
+      },
+      {
+        label: "2011",
+        value: "90.75",
+      },
+      {
+        label: "2012",
+        value: "90.8",
+      },
+      {
+        label: "2013",
+        value: "91.16",
+      },
+      {
+        label: "2014",
+        value: "91.37",
+      },
+      {
+        label: "2015",
+        value: "91.66",
+      },
+      {
+        label: "2016",
+        value: "91.8",
+      },
+    ],
+  };
+
+  const chartConfigs = {
+    type: "line",
+    width: "100%",
+    height: "100%",
+    dataFormat: "json",
+    dataSource,
+  };
   const columns = [
     {
       title: () => <b style={{ fontSize: "18px" }}>Coin Name</b>,
@@ -285,13 +295,7 @@ const CryptoTable = () => {
         </div>
         <div>
           <Suspense fallback={<div>Loading...</div>}>
-            <ReactFC
-              type="line"
-              width="100%"
-              height="100%"
-              dataFormat="json"
-              dataSource={dataSource}
-            />
+            <ReactFC {...chartConfigs} />
           </Suspense>
         </div>
       </Modal>
