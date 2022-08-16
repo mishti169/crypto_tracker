71316cf13d09c1cc2b68898d8d0b59059202c36d
MANISHA <manishapanchal169@gmail.com>
2022-08-16 10:57:07 +0530

    feat: :sparkles: implement chart with static data

--- src/components/CryptoTable/CryptoTable.jsx
+++ src/components/CryptoTable/CryptoTable.jsx
@@ -1,8 +1,76 @@
 import { Select, Table, Modal } from "antd";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import FusionCharts from "fusioncharts";
+import ReactFusioncharts from "react-fusioncharts";
+import charts from "fusioncharts/fusioncharts.charts";
+import FusionTheme from "fusioncharts/themes/fusioncharts.theme.fusion";
+
 import "./CryptoTable.css";
 
+charts(FusionCharts);
+
+const dataSource = {
+  chart: {
+    caption: "Average Fastball Velocity",
+    yaxisname: "Velocity (in mph)",
+    subcaption: "[2005-2016]",
+    numbersuffix: " mph",
+    rotatelabels: "1",
+    setadaptiveymin: "1",
+    theme: "fusion",
+  },
+  data: [
+    {
+      label: "2005",
+      value: "89.45",
+    },
+    {
+      label: "2006",
+      value: "89.87",
+    },
+    {
+      label: "2007",
+      value: "89.64",
+    },
+    {
+      label: "2008",
+      value: "90.13",
+    },
+    {
+      label: "2009",
+      value: "90.67",
+    },
+    {
+      label: "2010",
+      value: "90.54",
+    },
+    {
+      label: "2011",
+      value: "90.75",
+    },
+    {
+      label: "2012",
+      value: "90.8",
+    },
+    {
+      label: "2013",
+      value: "91.16",
+    },
+    {
+      label: "2014",
+      value: "91.37",
+    },
+    {
+      label: "2015",
+      value: "91.66",
+    },
+    {
+      label: "2016",
+      value: "91.8",
+    },
+  ],
+};
 // if value is in Billion format then remove B
 // convert String to number
 // then do comparison
@@ -209,6 +277,15 @@ const CryptoTable = () => {
             <h3>Current-Price: {selectedCoin.currPrice}</h3>
             <h3>Market Capital: {selectedCoin.capital}</h3>
           </div>
+        </div>
+        <div>
+          <ReactFusioncharts
+            type="line"
+            width="100%"
+            height="100%"
+            dataFormat="json"
+            dataSource={dataSource}
+          />
         </div>
       </Modal>
     </div>
